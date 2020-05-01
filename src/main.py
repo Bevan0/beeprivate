ff3dcb8351c706254ae70c186aefd3f973f5f743
Bevan <43471745+Bevan0@users.noreply.github.com>
2020-05-02 10:39:13 +1200

    Fix formatting of lightswitch_status

--- src/main.py
+++ src/main.py
@@ -21,17 +21,17 @@ async def test_page(req):
 async def lightswitch_status(req):
     return json({
         "serviceInstanceId": "fortnite",
-			"status": "UP",
-			"message": "WIP epic gamers",
-			"maintenanceUri": None,
-			"overrideCatalogIds": ["a7f138b2e51945ffbfdacc1af0541053"],
-			"allowedActions": ["PLAY", "DOWNLOAD"],
-			"banned": False,
-			"launcherInfoDTO": {
-				"appName": "Fortnite",
-				"catalogItemId": "4fe75bbc5a674f4f9b356b5c90567da5",
-				"namespace": "fn"
-			}
+        "status": "UP",
+        "message": "WIP epic gamers",
+        "maintenanceUri": None,
+        "overrideCatalogIds": ["a7f138b2e51945ffbfdacc1af0541053"],
+        "allowedActions": ["PLAY", "DOWNLOAD"],
+        "banned": False,
+        "launcherInfoDTO": {
+            "appName": "Fortnite",
+            "catalogItemId": "4fe75bbc5a674f4f9b356b5c90567da5",
+            "namespace": "fn"
+        }
     })
 
 
